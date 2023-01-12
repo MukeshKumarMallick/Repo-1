778e288e1598e79d85773b4e732b9d5c2e05e733
SunilDalei <sunildaleijitu@gmail.com>
2023-01-12 23:47:40 +0530

    Static print method added

--- main.java
+++ main.java
@@ -2,6 +2,10 @@ package myPack;
 
 class first
 {
+    static void print()
+    {
+        System.out.println("static print method called ");
+    }
     public static void main(String args[])
     {
         System.out.println("Hello Git!");
